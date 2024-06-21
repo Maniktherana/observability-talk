c2cb7977ef92512b52acf02d4764eefe9d060678
Manik Rana <manikrana54@gmail.com>
2024-06-21 10:38:21 +0530

    feat: add otel

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,8 +2,10 @@ import express from "express";
 import { Request, Response } from "express";
 
 import { metricsMiddleware, metricsRouter } from "./metrics";
-const { createLogger, format } = require("winston");
-const LokiTransport = require("winston-loki");
+import { metrics } from "@opentelemetry/api";
+
+import { createLogger, format } from "winston";
+import LokiTransport from "winston-loki";
 
 const app = express();
 
@@ -12,7 +14,7 @@ const logger = createLogger({
   format: format.combine(format.timestamp(), format.json()),
   transports: [
     new LokiTransport({
-      host: "http://host.docker.internal:3100",
+      host: "http://loki:3100",
       labels: { job: "express-api" },
       json: true,
       onConnectionError: (err: any) =>
@@ -26,6 +28,17 @@ app.use(express.json());
 
 app.use(metricsRouter);
 
+// Middleware to collect metrics
+app.use((req, res, next) => {
+  const meter = metrics.getMeter("express-server");
+
+  // Example: Counter for total requests to any route
+  const totalRequestsCounter = meter.createCounter("total_requests");
+  totalRequestsCounter.add(1);
+  logger.info("totalRequestsCounter", totalRequestsCounter);
+  next();
+});
+
 app.get("/", (req: Request, res: Response) => {
   logger.info("hello world");
   res.send("Hello, World!");
@@ -33,12 +46,18 @@ app.get("/", (req: Request, res: Response) => {
 
 app.get("/latency/fast", (req: Request, res: Response) => {
   logger.info("fast response");
+  const meter = metrics.getMeter("express-server");
+  const fastLatencyCounter = meter.createCounter("latency_fast_requests");
+  fastLatencyCounter.add(1);
   res.send("Fast response");
 });
 
 app.get("/latency/medium", (req: Request, res: Response) => {
   setTimeout(() => {
     logger.info("medium response");
+    const meter = metrics.getMeter("express-server");
+    const mediumLatencyCounter = meter.createCounter("latency_medium_requests");
+    mediumLatencyCounter.add(1);
     res.send("Medium response");
   }, 1000);
 });
@@ -46,6 +65,9 @@ app.get("/latency/medium", (req: Request, res: Response) => {
 app.get("/latency/slow", (req: Request, res: Response) => {
   setTimeout(() => {
     logger.info("slow response");
+    const meter = metrics.getMeter("express-server");
+    const slowLatencyCounter = meter.createCounter("latency_slow_requests");
+    slowLatencyCounter.add(1);
     res.send("Slow response");
   }, 3000);
 });
