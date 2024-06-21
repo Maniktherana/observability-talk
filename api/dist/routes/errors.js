b35fc0adc06da6abd59d2560b68e88ca81523e11
Manik Rana <manikrana54@gmail.com>
2024-06-21 08:33:07 +0530

    chore: add .gitignore

--- api/dist/routes/errors.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router.get("/crash", (req, res) => {
-    throw new Error("This is a crash!");
-});
-exports.default = router;
